8dd1882414a469b15b30d03231b0723568e9eee7
Azat Mukhametzyanov <amuhametzyanov@vtb.ru>
2022-05-21 13:14:23 +0300

    first lesson

--- 01. Type variable/src/ru/phoenigm/java/course/Main.java
+++ 01. Type variable/src/ru/phoenigm/java/course/Main.java
@@ -1,8 +1,28 @@
 package ru.phoenigm.java.course;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Scanner in = new Scanner(System.in);
+
+        int size = in.nextInt();
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Введите число: ");
+            array[i] = in.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+            sum = sum + array[i];
+            System.out.println("Текущая сумма: " + sum);
+
+        }
+        System.out.println("Результат: " + sum);
     }
 }
