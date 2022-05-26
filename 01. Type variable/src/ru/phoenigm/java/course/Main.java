0974380f6c3b74497042a2464e044d167ddaae75
Azat Mukhametzyanov <amuhametzyanov@vtb.ru>
2022-05-26 21:10:55 +0300

    third lesson

--- 01. Type variable/src/ru/phoenigm/java/course/Main.java
+++ 01. Type variable/src/ru/phoenigm/java/course/Main.java
@@ -6,23 +6,51 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
 
-        int size = in.nextInt();
-        int[] array = new int[size];
+        Scanner scanner = new Scanner(System.in);
+        int sizeArray = scanner.nextInt();
+
+        int[] array = new int[sizeArray];
+        printArray(array);
 
         for (int i = 0; i < array.length; i++) {
             System.out.println("Введите число: ");
-            array[i] = in.nextInt();
+            array[i] = scanner.nextInt();
         }
 
+        System.out.println("После заполнения массива пользователем: ");
+        printArray(array);
+        // строчки с 25 по 29 должны переместиться из main и замениться на вызов функции
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-            sum = sum + array[i];
+            sum = sum(sum, array[i]);
             System.out.println("Текущая сумма: " + sum);
-
         }
         System.out.println("Результат: " + sum);
+
+        System.out.println(sum(100, 200));
     }
+
+
+    static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Текущий элемент в индексе i=" + i + " array[i]=" + array[i]);
+        }
+    }
+
+    static int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    /**
+     * @param array входная переменная, в которой передается массив
+     * @return сумма элементов всего переданного массива
+     */
+    static int sumArray(int[] array) {
+        int sum = 0;
+        // код
+        return sum;
+    }
+
 }
